2f006796dc1f6331ba07694b46b162ffd72af786
Andrew Seales <agxs@frozenmercury.co.uk>
2012-06-27 23:32:18 +0100

    Added in proper size, and an easy d6 calculator.

--- scripts/planet.py
+++ scripts/planet.py
@@ -5,9 +5,17 @@ import traceback
 import random
 
 size = {
-  1: 'Small Size',
-  2: 'Medium Size',
-  3: 'Large Size'
+  0: '800 km',
+  1: '1600 km',
+  2: '3200 km',
+  3: '4800 km',
+  4: '6400 km',
+  5: '8000 km',
+  6: '9600 km',
+  7: '11200 km',
+  8: '12800 km',
+  9: '14400 km',
+  10: '16000 km'
 }
 
 techLevel = {
@@ -24,7 +32,7 @@ government = {
 
 class Planet:
   def __init__( self ):
-    self.size = random.randint( 1, 3 )
+    self.size = d6() + d6() - 2;
     self.techLevel = random.randint( 1, 3 )
     self.government = random.randint( 1, 3 )
 
@@ -34,10 +42,10 @@ class Planet:
            "Government: " + government[self.government]
 
   def planetCode( self ):
-    a = ord('A') + self.size - 1
+    a = "%X" % self.size
     b = ord('A') + self.techLevel - 1
     c = ord('A') + self.government - 1
-    return chr(a) + chr(b) + chr(c)
+    return a + chr(b) + chr(c)
  
 def main(*args):
   try:
@@ -51,6 +59,9 @@ def main(*args):
     return 1  # exit on error
   else:
     return 0  # exit errorlessly
+
+def d6():
+  return random.randint( 1, 6 )
 
 if __name__ == '__main__':
   sys.exit(main(*sys.argv))

a376d133d6acb1a298258fbacf9cf0082ed36075
Andrew Seales <agxs@frozenmercury.co.uk>
2012-06-29 15:03:42 +0100

    Requesting an invalid planet location now returns a 404 instead of an error.

--- server/planetserve/views.py
+++ server/planetserve/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from planetserve.models import Planet
 import json
 
@@ -13,4 +14,5 @@ def index( request ):
   return HttpResponse( json.dumps( jsonList ), content_type="application/json; charset=utf-8" )
 
 def detail( request, planet_id ):
-  return HttpResponse( json.dumps( Planet.objects.get( location__exact=planet_id ).toJSON() ), content_type="application/json; charset=utf-8" )
+  p = get_object_or_404( Planet, location__exact=planet_id )
+  return HttpResponse( json.dumps( p.toJSON() ), content_type="application/json; charset=utf-8" )

1f2fbe095932a0ca9873ab0a0c4be7bbbae2ffbe
Andrew Seales <agxs@frozenmercury.co.uk>
2012-06-29 14:39:26 +0100

    Planet JSON response now uses the correct ContentType and charset.

--- server/planetserve/views.py
+++ server/planetserve/views.py
@@ -10,7 +10,7 @@ def index( request ):
   for i in planetList:
     jsonList.append( i.toJSON() )
 
-  return HttpResponse( json.dumps( jsonList ) )
+  return HttpResponse( json.dumps( jsonList ), content_type="application/json; charset=utf-8" )
 
 def detail( request, planet_id ):
-  return HttpResponse( json.dumps( Planet.objects.get( location__exact=planet_id ).toJSON() ) )
+  return HttpResponse( json.dumps( Planet.objects.get( location__exact=planet_id ).toJSON() ), content_type="application/json; charset=utf-8" )

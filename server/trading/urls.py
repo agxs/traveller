86318e688433a0c4dfb50665b7620cc0eecf8dd6
Andrew Seales <agxs@frozenmercury.co.uk>
2012-07-07 17:30:06 +0100

    Added the first few pages:
      * The initial page, which collects some information.
      * POSTs to a data collection/processing page, which stores data in the
        session/
      * The passenger page.

--- server/trading/urls.py
+++ server/trading/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
+    url(r'^$', 'trading.views.index'),
+    url(r'^receiveInitial$', 'trading.views.receiveInitial'),
+    url(r'^rollForPassengers$', 'trading.views.rollForPassengers'),
+    
     url(r'^mock$', 'trading.views.mock'),
 )
--- server/trading/views.py
+++ server/trading/views.py
@@ -1,5 +1,26 @@
 from django.template import Context, loader
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from utils.dice import d6
+
+def index( request ):
+  t = loader.get_template( 'trading/index.html' )
+  c = Context( {} )
+  return HttpResponse( t.render( c ) )
+
+def receiveInitial( request ):
+  request.session['startPlanet'] = request.POST.get( 'startPlanet' )
+  request.session['destinationPlanet'] = request.POST.get( 'destinationPlanet' )
+  request.session['availableCargo'] = request.POST.get( 'availableCargo' )
+  request.session['numStateRooms'] = request.POST.get( 'numStateRooms' )
+  request.session['addMail'] = request.POST.get( 'addMail' )
+  
+  return redirect( '/trading/rollForPassengers' )
+
+def rollForPassengers( request ):
+  t = loader.get_template( 'trading/rollForPassengers.html' )
+  c = Context( {} )
+  return HttpResponse( t.render( c ) )
 
 def mock( request ):
   t = loader.get_template( 'trading/mock.html' )

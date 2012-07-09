6170bfe0840c70402ae31e200e81592914435c55
Andrew Seales <andrew.seales@ed.ac.uk>
2012-07-09 17:39:42 +0100

    Added all the trade goods as data items.

--- server/trading/speculative.py
+++ server/trading/speculative.py
@@ -93,7 +93,271 @@ trade_goods = {
       'Na': 3,
       'Ag': 2,
     } ),
+  'basicManufacturedGoods':
+    TradeGood( 'basicManufacturedGoods', '1d6*10', 10000, {
+      'Na': 2,
+      'In': 5,
+    }, {
+      'Ni': 3,
+      'Hi': 2,
+    } ),
+  'basicRawMaterials':
+    TradeGood( 'basicRawMaterials', '1d6*10', 5000, {
+      'Ag': 3,
+      'Ga': 2,
+    }, {
+      'In': 2,
+      'Po': 2,
+    } ),
+  'basicConsumables':
+    TradeGood( 'basicConsumables', '1d6*10', 2000, {
+      'Ag': 3,
+      'Wa': 2,
+      'Ga': 1,
+      'As': -4,
+    }, {
+      'As': 1,
+      'Fl': 1,
+      'Ic': 1,
+      'Hi': 1,
+    } ),
+  'basicOre':
+    TradeGood( 'basicOre', '1d6*10', 1000, {
+      'As': 4,
+    }, {
+      'In': 3,
+      'Ni': 1,
+    } ),
+  'advancedElectronics':
+    TradeGood( 'advancedElectronics', '1d6*5', 100000, {
+      'In': 2,
+      'Ht': 3,
+    }, {
+      'Ni': 1,
+      'Ri': 2,
+      'As': 3,
+    } ),
+  'advancedMachineParts':
+    TradeGood( 'advancedMachineParts', '1d6*5', 75000, {
+      'In': 2,
+      'Ht': 1,
+    }, {
+      'As': 2,
+      'Ni': 1,
+    } ),
+  'advancedManufacturedGoods':
+    TradeGood( 'advancedManufacturedGoods', '1d6*5', 100000, {
+      'In': 1,
+    }, {
+      'Hi': 1,
+      'Ri': 2,
+    } ),
+  'advancedWeapons':
+    TradeGood( 'advancedWeapons', '1d6*5', 150000, {
+      'Ht': 2,
+    }, {
+      'Po': 1,
+      'Az': 2,
+      'Rz': 4,
+    } ),
+  'advancedVehicles':
+    TradeGood( 'advancedVehicles', '1d6*5', 180000, {
+      'Ht': 2,
+    }, {
+      'As': 2,
+      'Ri': 2,
+    } ),
+  'biochemicals':
+    TradeGood( 'biochemicals', '1d6*5', 50000, {
+      'Ag': 1,
+      'Wa': 2,
+    }, {
+      'In': 2,
+    } ),
+  'crystalsAndGems':
+    TradeGood( 'crystalsAndGems', '1d6*5', 20000, {
+      'As': 2,
+      'De': 1,
+      'Ic': 1,
+    }, {
+      'In': 3,
+      'Ri': 2,
+    } ),
+  'cybernetics':
+    TradeGood( 'cybernetics', '1d6', 10000, {
+    }, {
+      'Ht': 1,
+      'Ic': 1,
+      'Ri': 2,
+    } ),
+  'liveAnimals':
+    TradeGood( 'liveAnimals', '1d6*10', 10000, {
+      'Ag': 2,
+    }, {
+      'Lo': 3,
+    } ),
+  'luxuryConsumables':
+    TradeGood( 'luxuryConsumables', '1d6*10', 20000, {
+      'Ag': 2,
+      'Wa': 1,
+    }, {
+      'Ri': 2,
+      'Hi': 2,
+    } ),
+  'luxuryGoods':
+    TradeGood( 'luxuryGoods', '1d6', 200000, {
+    }, {
+      'Ri': 4,
+    } ),
+  'medicalSupplies':
+    TradeGood( 'medicalSupplies', '1d6*5', 50000, {
+      'Ht': 2,
+    }, {
+      'In': 2,
+      'Po': 1,
+      'Ri': 1,
+    } ),
+  'petrochemicals':
+    TradeGood( 'petrochemicals', '1d6', 100000, {
+      'De': 2,
+    }, {
+      'In': 2,
+      'Ag': 1,
+      'Lo': 2
+    } ),
+  'pharmaceuticals':
+    TradeGood( 'pharmaceuticals', '1d6', 100000, {
+      'As': 2,
+      'Hi': 1,
+    }, {
+      'Ri': 2,
+      'Lo': 1,
+    } ),
+  'polymers':
+    TradeGood( 'polymers', '1d6*10', 7000, {
+    }, {
+      'Ri': 2,
+      'Ni': 1,
+    } ),
+  'preciousMetals':
+    TradeGood( 'preciousMetals', '1d6', 50000, {
+      'As': 3,
+      'De': 1,
+      'Ic': 2,
+    }, {
+      'Ri': 3,
+      'In': 2,
+      'Ht': 1
+    } ),
+  'radioactives':
+    TradeGood( 'radioactives', '1d6', 1000000, {
+      'As': 2,
+      'Lo': -4,
+    }, {
+      'In': 3,
+      'Ht': 1,
+      'Ni': -2,
+      'Ag': -3,
+    } ),
+  'robots':
+    TradeGood( 'robots', '1d6*5', 400000, {
+    }, {
+      'Ag': 2,
+      'Ht': 1,
+    } ),
+  'spices':
+    TradeGood( 'spices', '1d6*5', 6000, {
+      'De': 2,
+    }, {
+      'Hi': 2,
+      'Ri': 3,
+      'Po': 3,
+    } ),
+  'textiles':
+    TradeGood( 'textiles', '1d6*10', 3000, {
+      'Ag': 7,
+    }, {
+      'Hi': 3,
+      'Na': 2,
+    } ),
+  'uncommonOre':
+    TradeGood( 'uncommonOre', '1d6*10', 5000, {
+      'As': 4,
+    }, {
+      'In': 3,
+      'Ni': 1,
+    } ),
+  'uncommonRawMaterials':
+    TradeGood( 'uncommonRawMaterials', '1d6*10', 20000, {
+      'Ag': 2,
+    }, {
+      'In': 2,
+      'Ht': 1,
+    } ),
+  'wood':
+    TradeGood( 'wood', '1d6*10', 1000, {
+      'Ag': 6,
+    }, {
+      'Ri': 2,
+      'In': 1,
+    } ),
+  'vehicles':
+    TradeGood( 'vehicles', '1d6*10', 15000, {
+      'In': 2,
+      'Ht': 1,
+    }, {
+      'Ni': 2,
+      'Hi': 1,
+    } ),
+  'illegalBiochemicals':
+    TradeGood( 'illegalBiochemicals', '1d6*5', 50000, {
+      'Wa': 2,
+    }, {
+      'In': 6,
+    } ),
+  'illegalCybernetics':
+    TradeGood( 'illegalCybernetics', '1d6', 250000, {
+    }, {
+      'As': 4,
+      'Ic': 4,
+      'Ri': 8,
+      'Az': 6,
+      'Rz': 6,
+    } ),
+  'illegalDrugs':
+    TradeGood( 'illegalDrugs', '1d6', 100000, {
+    }, {
+      'Ri': 6,
+      'Hi': 6,
+    } ),
+  'illegalLuxuries':
+    TradeGood( 'illegalLuxuries', '1d6', 50000, {
+      'Ag': 2,
+      'Wa': 1,
+    }, {
+      'Ri': 6,
+      'Hi': 4,
+    } ),
+  'illegalWeapons':
+    TradeGood( 'illegalWeapons', '1d6*5', 150000, {
+      'Ht': 2,
+    }, {
+      'Po': 6,
+      'Az': 8,
+      'Rz': 10,
+    } ),
 }
+'''
+template
+  '':
+    TradeGood( '', '', 0, {
+      '': 0,
+      '': 0,
+    }, {
+      '': 0,
+      '': 0,
+    } ),
+'''
 #13 Basic Manufactured
 #Goods
 #All 1d6 ∞ 10 10,000 Non-Agricultural +2,
